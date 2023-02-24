762545102a3ddc54764ac97f83694d712e3bbd4a
Daniil Dergunov <100157798+Rogepta@users.noreply.github.com>
2023-02-24 17:47:52 +0300

    [FEATURE] Add functional: remove, filter, add new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v1 } from "uuid";
 import "./App.css";
 import { TaskType, Todolist } from "./Todolist";
 
@@ -6,17 +7,27 @@ export type FilterValuesType = "all" | "active" | "complited";
 
 function App() {
   let [tasks, setTasks] = useState<Array<TaskType>>([
-    { id: 1, title: "Blog", isDone: true },
+    { id: v1(), title: "Blog", isDone: true },
 
-    { id: 2, title: "Landing", isDone: false },
+    { id: v1(), title: "Landing", isDone: false },
 
-    { id: 3, title: "Chat", isDone: true },
+    { id: v1(), title: "Chat", isDone: true },
+
+    { id: v1(), title: "Resume", isDone: false },
+
+    { id: v1(), title: "Pet Project", isDone: false },
   ]);
   let [filter, setFilter] = useState<FilterValuesType>("all");
 
-  function removeTask(id: number) {
+  function removeTask(id: string) {
     let filteredTasks = tasks.filter((t) => t.id !== id);
     setTasks(filteredTasks);
+  }
+
+  function addTask(title: string) {
+    let newTask = { id: v1(), title: title, isDone: false };
+    let newTasks = [newTask, ...tasks];
+    setTasks(newTasks);
   }
 
   function changeFilter(value: FilterValuesType) {
@@ -38,6 +49,7 @@ function App() {
         tasks={tasksForToDoList}
         removeTask={removeTask}
         changeFilter={changeFilter}
+        addTask={addTask}
       />
     </div>
   );
